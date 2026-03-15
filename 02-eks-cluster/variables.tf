fbd96e89ca436feccb9092de51bc63c21cb4018a
Marcelo Gomes <mgsantos@gmail.com>
2026-03-15 14:21:45 -0300

    update

---
+++ 02-eks-cluster/variables.tf
@@ -0,0 +1,19 @@
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "production"
+    Project     = "workshop-devops-na-nuvem"
+  }
+}
+
+variable "assume_role" {
+  type = object({
+    arn    = string
+    region = string
+  })
+
+  default = {
+    arn    = "arn:aws:iam::631918221338:role/workshop-march-role"
+    region = "us-east-1"
+  }
+}
